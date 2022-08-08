c5a78e9bb942d7b1eb72d3439d59ad6f515ef21b
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-08 20:51:42 +0200

    Changes in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,84 @@
-import { defineNuxtConfig } from 'nuxt'
+import {defineNuxtConfig} from "nuxt"
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+
+    app: {
+
+        head: {
+            title: process.env.title,
+            meta: [
+                { 
+                    charset: "utf-8" 
+                },
+                { 
+                    "http-equiv": "X-UA-Compatible", 
+                    content: "IE=edge" 
+                },
+                {
+                    name: "viewport", 
+                    content: "width=device-width, initial-scale=1" 
+                },
+                { 
+                    hid: "description", 
+                    name: "description", 
+                    content: process.env.description 
+                },
+                { 
+                    hid: "robots", 
+                    name: "robots", 
+                    content: "index,follow" 
+                },
+                { 
+                    hid: "theme-color", 
+                    name: "theme-color", 
+                    content: process.env.color 
+                }
+            ],
+            script: [
+                {
+                    src: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.min.js"
+                }
+            ],
+            link: [
+                { 
+                    rel: "stylesheet", 
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/css/bootstrap.min.css" 
+                },
+                {
+                    rel: "stylesheet",
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+                },
+                {
+                    rel: "stylesheet",
+                    href: "https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+                }
+            ],
+            noscript: [
+                {
+                    children: "Javascript is required" 
+                }
+            ]
+        },
+
+    },
+
+    css: [
+        "~/assets/css/general.css"
+    ],
+
+    ssr: false,
+
+    build: {
+        extractCSS: true
+    },
+
+    publicRuntimeConfig: {
+        env: {
+            title: process.env.title,
+            header: process.env.header,
+            api: process.env.api
+        }
+    }
 
 })
