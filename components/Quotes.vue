d3f2616952ed3ab992ca036bac4ad3aded9e7ba9
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-09 12:18:40 +0200

    Added quotes slider

---
+++ components/Quotes.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="carouselQuoteCaptions" class="carousel mt-3" data-bs-ride="carousel" data-bs-interval="300000">
+        <div v-for="(item, i) of randomized" :key="i" v-bind:class="i ? null : 'active'" class="carousel-item">
+            <div class="carousel-caption">
+                <blockquote class="blockquote border-0 p-0">
+                    <p class="font-italic lead"> <i class="fa fa-quote-left mr-3 text-colored"></i> {{item.quote}}</p>
+                    <footer class="blockquote-footer">{{item.author}}</footer>
+                </blockquote>
+            </div>
+        </div>
+        <div class="carousel-indicators">
+            <button v-for="(item, i) of randomized" :key="i" type="button" data-bs-target="#carouselQuoteCaptions" :data-bs-slide-to="i" v-bind:class="i ? null : 'active'" :aria-current="i ? true : false"></button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["quotes"],
+    data() {
+        return {
+            randomized: this.quotes.sort(() => Math.random() - 0.5)
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,20 @@
     <main>
         <Header :settings="obj" />
 
-            <div class="container">
+            <div class="container text-center">
+
                 <span class="regular">Are you still here?</span>
                 <div class="row text-center">
                     <div class="col-lg-8 col-md-12">
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare massa eget egestas purus viverra accumsan in nisl. Eget egestas purus viverra accumsan in nisl nisi scelerisque eu. Odio ut sem nulla pharetra diam sit amet nisl suscipit. Est lorem ipsum dolor sit amet consectetur. Elit scelerisque mauris pellentesque pulvinar pellentesque. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris. Augue eget arcu dictum varius duis at. Lorem ipsum dolor sit amet. Vitae tortor condimentum lacinia quis vel eros donec ac. Nibh ipsum consequat nisl vel pretium. Varius vel pharetra vel turpis nunc. Quam elementum pulvinar etiam non quam lacus. Dignissim enim sit amet venenatis urna. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Dignissim suspendisse in est ante in nibh mauris cursus mattis. Suspendisse interdum consectetur libero id. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Lectus proin nibh nisl condimentum id venenatis a condimentum.
                     </div>
                     <div class="col-lg-4 col-md-12">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare massa eget egestas purus viverra accumsan in nisl. Eget egestas purus viverra accumsan in nisl nisi scelerisque eu. Odio ut sem nulla pharetra diam sit amet nisl suscipit. Est lorem ipsum dolor sit amet consectetur. Elit scelerisque mauris pellentesque pulvinar pellentesque. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris. Augue eget arcu dictum varius duis at. Lorem ipsum dolor sit amet. Vitae tortor condimentum lacinia quis vel eros donec ac. Nibh ipsum consequat nisl vel pretium. Varius vel pharetra vel turpis nunc. Quam elementum pulvinar etiam non quam lacus. Dignissim enim sit amet venenatis urna. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Dignissim suspendisse in est ante in nibh mauris cursus mattis. Suspendisse interdum consectetur libero id. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Lectus proin nibh nisl condimentum id venenatis a condimentum.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare massa eget egestas purus viverra accumsan in nisl. Eget egestas purus viverra accumsan in nisl nisi scelerisque eu. Odio ut sem nulla pharetra diam sit amet nisl suscipit. Est lorem ipsum dolor sit amet consectetur. Elit scelerisque mauris pellentesque pulvinar pellentesque. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum.
                     </div>
                 </div>
+
+                <Quotes :quotes="obj.quotes" />
+
             </div>
 
         <Footer />
