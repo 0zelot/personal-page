ec5e18322d8acf4f71a2ade7e5e96831522fcde1
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-13 20:48:05 +0200

    Added particles

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <header>
-        
+        <Particles class="position-absolute h-100 w-100" id="tsparticles" :options="options" :particles-init="particlesInit" />
+
         <div class="center">
 
             <span class="regular">Hello stranger, i'm</span>
@@ -14,7 +15,7 @@
                 </div>
             </div>
 
-            <div class="icons favourite row text-center mt-4">
+            <div v-if="settings.favourites && settings.favourites.length > 0" class="icons favourite row text-center mt-4">
                 <h2 class="m-3 regular">What do I use</h2>
                 <div v-for="(item, i) of settings.favourites" :key="i" class="col">
                     <i :class="`${item} colored`"></i>
@@ -27,11 +28,113 @@
 </template>
 
 <script>
+import {loadFull} from "tsparticles";
+
 export default {
     props: ["settings"],
     data() {
         return {
-            header: this.$config.env.header.replaceAll(/["']/g, "")
+            header: this.$config.env.header.replaceAll(/["']/g, ""),
+            options: {
+                fullScreen: {
+                enable: false,
+                zIndex: 1
+            },
+            particles: {
+                number: {
+                    value: 170,
+                    density: {
+                    enable: true,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: "ffffff"
+            },
+            shape: {
+                type: "circle"
+            },
+            opacity: {
+                value: 0.7,
+                random: true,
+                    anim: {
+                    enable: true,
+                    speed: 0.7,
+                    opacity_min: 0,
+                    sync: false
+                }
+            },
+            size: {
+                value: 3,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 4,
+                    size_min: 0.3,
+                    sync: false
+                }
+            },
+            move: {
+                enable: true,
+                speed: 0.7,
+                direction: "none",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 600
+                }
+            },
+            interactivity: {
+                events: {
+                    onhover: {
+                        enable: false,
+                        mode: "bubble"
+                    },
+                    onclick: {
+                        enable: false,
+                        mode: "repulse"
+                    },
+                    resize: false
+                },
+                modes: {
+                    grab: {
+                        distance: 400,
+                        line_linked: {
+                            opacity: 1
+                        }
+                    },
+                    bubble: {
+                        distance: 250,
+                        size: 0,
+                        duration: 2,
+                        opacity: 0,
+                        speed: 3
+                    },
+                    repulse: {
+                        distance: 400,
+                        duration: 0.4
+                    },
+                    push: {
+                        particles_nb: 4
+                    },
+                    remove: {
+                        particles_nb: 2
+                    }
+                }
+            },
+            retina_detect: true,
+            background: {
+                color: "#232741",
+            }}},
+        }
+    },
+    methods: {
+        particlesInit: async (engine) => {
+            await loadFull(engine);
         }
     }
 }
