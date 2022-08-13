3922a666982fb830b840fbf1922ba62e7c9fed0a
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-13 20:48:48 +0200

    Modified quotes slider

--- components/Quotes.vue
+++ components/Quotes.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="carouselQuoteCaptions" class="carousel mt-3" data-bs-ride="carousel" data-bs-interval="300000">
-        <div v-for="(item, i) of randomized" :key="i" v-bind:class="i ? null : 'active'" class="carousel-item">
+    <div id="carouselQuotes" class="carousel mt-3 carousel-fade" data-bs-ride="carousel" data-bs-interval="8000">
+        <div v-for="(item, i) of quotes" :key="i" v-bind:class="i ? null : 'active'" class="carousel-item">
             <div class="carousel-caption">
                 <blockquote class="blockquote border-0 p-0">
                     <p class="font-italic lead"> <i class="fa fa-quote-left mr-3 text-colored"></i> {{item.quote}}</p>
@@ -9,7 +9,7 @@
             </div>
         </div>
         <div class="carousel-indicators">
-            <button v-for="(item, i) of randomized" :key="i" type="button" data-bs-target="#carouselQuoteCaptions" :data-bs-slide-to="i" v-bind:class="i ? null : 'active'" :aria-current="i ? true : false"></button>
+            <button v-for="(item, i) of quotes" :key="i" type="button" data-bs-target="#carouselQuotes" :data-bs-slide-to="i" v-bind:class="i ? null : 'active'" :aria-current="i ? true : false"></button>
         </div>
     </div>
 </template>
@@ -17,10 +17,8 @@
 <script>
 export default {
     props: ["quotes"],
-    data() {
-        return {
-            randomized: this.quotes.sort(() => Math.random() - 0.5)
-        }
+    mounted() {
+        new bootstrap.Carousel(document.querySelector("#carouselQuotes"));
     }
 }
 </script>
