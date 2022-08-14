df932882f2c272e070a22db88118cc62dd0abd3a
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-14 16:27:33 +0200

    Added custom error page

---
+++ error.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div class="center">
+            <hgroup>
+                <h1 class="title">{{error.statusCode}}</h1>
+                <h2 class="h3 text-muted">{{mappings[error.statusCode] || error.message}}</h2>
+            </hgroup>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    props: ["error"],
+    layout: "error",
+    data() {
+        return {
+            mappings: {
+                404: "Page not found.",
+                503: "Service is temporarily unavailable.",
+                504: "API server is unreachable."
+            }
+        }
+    }
+}
+</script>
