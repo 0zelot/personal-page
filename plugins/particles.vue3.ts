84ce470ad98c6f3817efabfd19141e6624ba393d
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-16 16:51:38 +0200

    Some changes

--- plugins/particles.vue3.ts
+++ plugins/particles.vue3.ts
@@ -1,6 +1,5 @@
 import Particles from "particles.vue3";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  // Doing something with nuxtApp
-  nuxtApp.vueApp.use(Particles);
+    nuxtApp.vueApp.use(Particles);
 });
