e1701699d8a207321bfa9e3d0a822b4b03913c00
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-24 11:32:15 +0200

    Some changes in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <footer class="text-center">
-        <span class="align">Copyright {{new Date().getFullYear()}} | Made by <a href="https://github.com/0zelot">Ozelot</a>.</span>
-        <span class="add-label"><a href="https://www.freepik.com/vectors/cartoon-space">Cartoon space vector</a> created by vectorpouch - <a href="https://freepik.com">freepik.com</a>.</span>
+    <footer class="text-center m-1">
+        <span class="align">Copyright {{new Date().getFullYear()}} | Created using <i class="devicon-nuxtjs-plain colored"></i> by <a href="https://ozelot.ovh" target="_blank">Ozelot</a>.</span>
+        <span v-if="useRoute().path == '/'" class="add-label"><a href="https://www.freepik.com/vectors/cartoon-space" target="_blank">Cartoon space vector</a> created by vectorpouch - <a href="https://freepik.com" target="_blank">freepik.com</a>.</span>
     </footer>
 </template>
