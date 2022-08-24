44b743998bed4b306f632091f72c025492e92717
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-24 11:33:11 +0200

    Added table of contents

---
+++ components/Toc.vue
@@ -0,0 +1,24 @@
+<template>
+    <aside v-if="links.length > 2">
+        <h3 class="h5 font-bold">Table of contents</h3>
+        <ul>
+            <li v-for="(item, i) of links" :key="i" class="toc-item">
+                <a :href="`#${item.id}`">{{item.text}}</a>
+                <ul v-if="item.children && item.children.length > 0">
+                    <span v-for="(subitem, j) of item.children" :key="j">
+                        <li v-if="subitem.text" class="toc-item">
+                            <a :href="`#${subitem.id}`">{{subitem.text}}</a>
+                        </li>
+                    </span>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</template>
+
+<script setup>
+const props = defineProps({
+    links: Object,
+});
+const {links} = toRefs(props);
+</script>
