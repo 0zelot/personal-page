f74a90473d77fc6baad1d894389bcbe9bfb5d406
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-08-24 11:35:00 +0200

    Adjusted rss feed to Nuxt Content

--- server/routes/rss.xml.ts
+++ server/routes/rss.xml.ts
@@ -1,10 +1,9 @@
+import {serverQueryContent} from "#content/server";
 import {Feed} from "feed";
-import fetch from "node-fetch";
-import showdown from "showdown";
-
-const converter = new showdown.Converter();
 
 export default defineEventHandler(async (event) => {
+
+    const posts = await serverQueryContent(event).where({visibility: 1}).find();
 
     const feed = new Feed({
         id: "rss",
@@ -15,21 +14,26 @@ export default defineEventHandler(async (event) => {
         copyright: `Copyright ${new Date().getFullYear()} - ${process.env.title}`
     });
 
-    const config = await (await (fetch(`${process.env.api}/config.json`))).json();
-
-    for(const post of config.blog) {
+    for(const post of posts) {
         feed.addItem({
             title: post.title,
-            id: `https://${process.env.domain}/blog/${post.slug}`,
-            link: `https://${process.env.domain}/blog/${post.slug}`,
+            id: `https://${process.env.domain}${post._path}`,
+            link: `https://${process.env.domain}${post._path}`,
             image: post.image,
             description: post.description,
             published: new Date(post.created * 1000),
-            content: converter.makeHtml(await (await fetch(`${process.env.api}${post.content}`)).text()),
+            content: `
+                <p>
+                    <img alt="Cover image" src="${post.image}">
+                </p>
+                <p>You are viewing an article as a preview for RSS readers and it may be slightly different from the original article. <a href='https://${process.env.domain}${post._path}'>Click here</a> to see full article.</p> 
+                ${post.excerpt.children.map(b => `<${b.tag} ${b.children.filter(x => x.type === 'element')}>${b.children.filter(x => x.type === 'text').map(c => c.value).join(' ')}</${b.tag}>`).join('')}
+            `,
             date: undefined
         })
     }
 
     appendHeader(event, "Content-Type", "application/xml");
     return feed.rss2();
+
 });
