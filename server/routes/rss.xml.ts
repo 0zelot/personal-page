82d99b1d3bfbd05be5815d56795f4c3cb695cf98
Ozelot <29740364+0zelot@users.noreply.github.com>
2022-10-28 20:36:08 +0200

    Fixed RSS content view

--- server/routes/rss.xml.ts
+++ server/routes/rss.xml.ts
@@ -1,6 +1,48 @@
 import {serverQueryContent} from "#content/server";
 import {Feed} from "feed";
 
+// Parsing function by szadoww - https://github.com/szadoww
+
+const parse = (json) => {
+
+    const type = json["type"];
+    const tag = json["tag"];
+    const children = json["children"];
+
+    let result = "";
+
+    if(tag) {
+        
+        result += `<${tag}`;
+        const props = json["props"];
+
+        if(props) {
+            for(const prop of Object.entries(props)) {
+                const key = prop[0];
+                const value = prop[1];
+
+                if(key == "code") continue;
+
+                result += ' ' + key + '="' + value + '"';
+            }
+        }
+
+        result += ">";
+    }
+
+    if(type == "text") result += json["value"];
+
+    if(children) {
+        for(const child of children) result += parse(child);
+    }
+
+    if(tag) result += `</${tag}>`;
+
+    return result
+        .replaceAll("\r\n", "<br>")
+        .replaceAll("\n", "<br>");
+}
+
 export default defineEventHandler(async (event) => {
 
     const posts = await serverQueryContent(event).where({visibility: 1}).find();
@@ -27,10 +69,11 @@ export default defineEventHandler(async (event) => {
                     <img alt="Cover image" src="${post.image}">
                 </p>
                 <p>You are viewing an article as a preview for RSS readers and it may be slightly different from the original article. <a href='https://${process.env.domain}${post._path}'>Click here</a> to see full article.</p> 
-                ${post.excerpt.children.map(b => `<${b.tag} ${b.children.filter(x => x.type === 'element')}>${b.children.filter(x => x.type === 'text').map(c => c.value).join(' ')}</${b.tag}>`).join('')}
+                ${parse(post.excerpt)}
+                <p>For more articles, visit <a href="https://${process.env.domain}">${process.env.domain}</a> or subscribe this RSS channel - <a href="https://${process.env.domain}/rss.xml">${process.env.domain}/rss.xml</a>.</p>
             `,
             date: undefined
-        })
+        });
     }
 
     appendHeader(event, "Content-Type", "application/xml");
